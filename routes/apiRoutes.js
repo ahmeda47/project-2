21e3d0e531686e811c0627db7662fd72da65ef23
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-02-12 20:32:13 -0500

    setup server and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+var db = require("../models");
+
+module.exports = function(app) {
+//   app.get("/", function(req, res) {
+//     // 1. Add a join to include all of each Author's Posts
+    
+//       res.sendFile("index.html");
+//     });
+  
+
+  app.get("/api/login", function(req, res) {
+    res.sendFile("login.html");
+    //   dbUsers.findOne({
+    //     where:{
+    //       userName:req.body.userName
+    //     }
+    //   })
+    // });
+  });
+  app.get("/api/signup", function(req,res){
+    res.sendFile("../public/signup.html");
+  });
+
+  // app.post("/api/signup", function(req, res) {
+  //   db.Users.create(req.body).then(function(dbUsers) {
+  //     res.json(dbUsers);
+  //   });
+
+  // });
+}
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
-const app = express();
-let PORT = 8080;
-app.use(express.static("public"));
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const routes = require("./controllers/controllers.js");
-app.use(routes);
-app.listen(PORT, err => {
-  if (err) throw err;
-  console.log(`connected to port http://localhost:${PORT}`);
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+// db.sequelize.sync({ force: true }).then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
+
+app.listen(PORT, function() {
+  console.log(`connected to http://localhost:${PORT}`);
 });
