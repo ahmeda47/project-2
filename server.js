edce0989c5ae0c1cc11c4f1f208307252e4d0741
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-02-12 19:24:57 -0500

    updated home page/server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+let PORT = 8080;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const routes = require("./controllers/controllers.js");
+app.use(routes);
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`connected to port http://localhost:${PORT}`);
+});
