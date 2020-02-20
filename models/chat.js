17e4d1bc73efc067aaa855837abebf98ada65ca1
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-02-19 22:14:15 -0500

    cleaned up files

--- models/chat.js
+++ models/chat.js
@@ -1,31 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
-    var Chat = sequelize.define("Chat", {
-   
-      Message: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });  
-    return Chat;
-}
-  
-  
   var Chat = sequelize.define("Chat", {
-    annotation_id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
     Message: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    userName: {
-      type: DataTypes.STRING
-      // references: {
-      //     model: 'User',
-      //     key: 'id'
-      // }
     }
   });
   return Chat;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,61 +5,7 @@ const saltRounds = 10;
 const path = require("path");
 require("dotenv").config();
 
-// module.exports = function(app) {
-//   //Root Route
-//   app.get("/", function(req, res) {
-//     res.sendFile("index.html");
-//   });
-
-//   // API Route for login
-//   app.get("/api/login", function(req, res) {
-//     res.sendFile("login.html");
-//     dbUsers.findOne({
-//       where: {
-//         userName: req.body.userName
-//       }
-//     });
-//   });
-
-//   // API Route for Signup
-//   app.get("/api/signup", function(req, res) {
-//     res.sendFile("../public/signup.html");
-//   });
-
-//   //Post User data to database
-//   app.post("/api/signup", function(req, res) {
-//     db.Users.create(req.body).then(function(dbUsers) {
-//       res.json(dbUsers);
-//     });
-//   });
-//   //API Route for ask question
-//   app.get("/api/ask-question", function(req, res) {
-//     res.sendFile("../public/buildQuestion.html");
-//   });
-//   // Api Route to post question data into database
-//   app.post("/api/ask-question", function(req, res) {
-//     db.Question.create(req.body.Title, req.body.Body).then(function(
-//       dbQuestion
-//     ) {
-//       res.json(dbQuestion);
-//     });
-//   });
-// };
-// app.get("/api/login", function(req, res) {
-//   db.User.findAll({}).then(function(dbUsers) {
-//     res.json(dbUsers);
-//   });
-// });
-
-//mine
-
 module.exports = function(app) {
-  app.get("/api/signup", function(req, res) {
-    db.User.findAll({}).then(function(dbUsers) {
-      res.json(dbUsers);
-    });
-  });
-
   app.get("/buildquestion", authenticationMiddleware(), function(req, res) {
     res.sendFile(path.join(__dirname, "../public/buildQuestion.html"));
     console.log(req.user);
@@ -100,31 +46,29 @@ module.exports = function(app) {
     done(null, user_id);
   });
   //API Route for ask question
-  app.get("/api/buildQuestion",function(req,res){
+  app.get("/api/buildQuestion", function(req, res) {
     res.sendFile("../public/buildQuestion.html");
   });
   // Api Route to post question data into database
-  app.post("/api/buildQuestion",function(req,res){
+  app.post("/api/buildQuestion", function(req, res) {
     // db.Question.create(req.body.Title, req.body.body).then(function(dbQuestion){
     //   res.json(dbQuestion);
     // });
 
-    console.log('hello')
-  })
-}
-=======
-
-  passport.deserializeUser(function(user_id, done) {
-    done(null, user_id);
+    console.log("hello");
   });
-  function authenticationMiddleware() {
-    return (req, res, next) => {
-      console.log(
-        `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
-      );
-
-      if (req.isAuthenticated()) return next();
-      res.redirect("/login");
-    };
-  }
 };
+
+passport.deserializeUser(function(user_id, done) {
+  done(null, user_id);
+});
+function authenticationMiddleware() {
+  return (req, res, next) => {
+    console.log(
+      `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
+    );
+
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+  };
+}
